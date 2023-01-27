14eb573294128176d289582c17838cf6ef1a0be2
Yuta Koshizawa <koher@koherent.org>
2023-01-28 00:25:45 +0900

    プロジェクトを作成

---
+++ CombineToAsyncSequence/CombineToAsyncSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineToAsyncSequenceApp.swift
+//  CombineToAsyncSequence
+//
+//  Created by Yuta Koshizawa on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct CombineToAsyncSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
