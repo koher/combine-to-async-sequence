5e05d47857afe4b08daae27e330dfc38de28b4be
Yuta Koshizawa <koher@koherent.org>
2023-01-28 01:42:11 +0900

    PlaygroundにSwift Async Algorithmsを追加

--- Playground/Package.swift
+++ Playground/Package.swift
@@ -5,17 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Playground",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v12)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Playground",
-            dependencies: []),
+            dependencies: [
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            ]),
         .testTarget(
             name: "PlaygroundTests",
             dependencies: ["Playground"]),
