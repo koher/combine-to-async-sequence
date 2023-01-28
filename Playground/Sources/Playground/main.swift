b68cfd8a863a2b0861b5bacaf719d8c48dfb1dd9
Yuta Koshizawa <koher@koherent.org>
2023-01-28 18:04:33 +0900

    @rethrowsの説明のためのコードを追加

--- Playground/Sources/Playground/main.swift
+++ Playground/Sources/Playground/main.swift
@@ -46,12 +46,137 @@ import Combine
 //}
 //print()
 
-let subject: CurrentValueSubject<Int, Never> = .init(42)
-var cancellables: Set<AnyCancellable> = []
+//let subject: CurrentValueSubject<Int, Never> = .init(42)
+//var cancellables: Set<AnyCancellable> = []
+//
+//subject.sink { value in
+//    print(value)
+//}
+//.store(in: &cancellables)
+//
+//subject.send(999)
 
-subject.sink { value in
-    print(value)
+struct GeneralError: Error {
 }
-.store(in: &cancellables)
 
-subject.send(999)
+@rethrows
+protocol P {
+    func foo() throws -> Int
+}
+
+@rethrows
+protocol Q {
+    associatedtype X: P
+
+    func bar() throws -> Int
+    func x() -> X
+}
+
+struct A: P {
+    func foo()  throws -> Int {
+        if Bool.random() { throw GeneralError() }
+        return 42
+    }
+}
+
+struct B: P {
+    func foo() -> Int {
+        42
+    }
+}
+
+struct C: Q {
+    func bar() throws -> Int {
+        if Bool.random() { throw GeneralError() }
+        return 999
+    }
+
+    func x() -> A {
+        A()
+    }
+}
+
+struct D: Q {
+    func bar() -> Int {
+        999
+    }
+
+    func x() -> B {
+        B()
+    }
+}
+
+//do {
+//    let p = A()
+//    print(try p.foo())
+//}
+//
+//do {
+//    let p = B()
+//    print(p.foo())
+//}
+
+func useP<X: P>(_ x: X) rethrows {
+    print(try x.foo())
+}
+
+try useP(A())
+useP(B())
+
+func useQ<Y: Q>(_ y: Y) rethrows {
+    print(try y.x().foo())
+}
+
+try useQ(C())
+useQ(D())
+
+
+@rethrows
+protocol MyAsyncIteratorProtocol<Element> {
+    associatedtype Element
+    mutating func next() async throws -> Element?
+}
+
+@rethrows
+protocol MyAsyncSequence<Element> {
+    associatedtype Element
+    associatedtype AsyncIterator: MyAsyncIteratorProtocol<Element>
+    func makeAsyncIterator() -> AsyncIterator
+}
+
+struct Pair<Element>: MyAsyncSequence {
+    var values: (Element, Element)
+    init(_ value0: Element, _ value1: Element) {
+        self.values = (value0, value1)
+    }
+    func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(self)
+    }
+    struct AsyncIterator: MyAsyncIteratorProtocol {
+        private let pair: Pair<Element>
+        private var index: Int = 0
+        init(_ pair: Pair<Element>) {
+            self.pair = pair
+        }
+        mutating func next() -> Element? {
+            defer { index += 1 }
+            switch index {
+            case 0: return pair.values.0
+            case 1: return pair.values.1
+            default: return nil
+            }
+        }
+    }
+}
+
+func forEach<T, S: MyAsyncSequence<T>>(of sequence: S, operation: @escaping (T) throws -> Void) async rethrows {
+    var iterator = sequence.makeAsyncIterator()
+    while let element = try await iterator.next() {
+        try operation(element)
+    }
+}
+
+let pair: Pair<Int> = .init(2, 3)
+await forEach(of: pair) { element in
+    print(element)
+}
